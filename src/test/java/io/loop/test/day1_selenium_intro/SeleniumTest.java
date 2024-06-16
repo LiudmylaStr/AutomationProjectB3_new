0033ce87e0d0e01b5d65f07fb7eebc3500194fe1
ludmyla <ludmila.str0405@gmail.com>
2024-06-16 12:54:00 -0400

    my second commit

--- src/test/java/io/loop/test/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/io/loop/test/day1_selenium_intro/SeleniumTest.java
@@ -1,4 +1,34 @@
 package io.loop.test.day1_selenium_intro;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumTest {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        //create an instance of webdriver
+        WebDriver driver = new ChromeDriver();
+        //maximise the window
+        driver.manage().window().maximize();
+  //driver.manage().window().fullscreen(); another way to maximize window
+        //navigate to the webpage
+        driver.get("https://www.loopcamp.io");
+        Thread.sleep(5000);
+        driver.navigate().to("https://www.etsy.com");
+
+        //navigate back
+
+        driver.navigate().back();
+
+        //navigate forward
+        driver.navigate().forward();
+
+        //refresh
+        driver.navigate().refresh();
+
+        //quit and close
+       // driver.close();
+        driver.quit();
+    }
 }
