3509efd1584161e070c8f2c43d2b95e53f4850a3
LiudmylaStrashko <ludmila.str0405@gmail.com>
2024-06-21 08:34:35 -0400

    get attribute

---
+++ src/test/java/io/loop/test/day4/T0_Forget_Password.java
@@ -0,0 +1,10 @@
+package io.loop.test.day4;
+/*
+    verify forget password
+    identify reser password heading using css and go from parent to child
+     */
+public class T0_Forget_Password {
+    public static void main(String[] args) {
+
+    }
+}
