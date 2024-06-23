93c948a4c47e817b7a92f187f87d5757acda082a
ludmyla <ludmila.str0405@gmail.com>
2024-06-23 09:41:05 -0400

    my second commit(day3 changes)

--- src/test/java/io/loop/test/day4/T0_Forget_Password.java
+++ src/test/java/io/loop/test/day4/T0_Forget_Password.java
@@ -1,10 +1,26 @@
 package io.loop.test.day4;
+
+import io.loop.test.utilities.WebDriverUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 /*
     verify forget password
-    identify reser password heading using css and go from parent to child
+    identify reset password heading using css and go from parent to child
      */
 public class T0_Forget_Password {
     public static void main(String[] args) {
+        WebDriver driver = WebDriverUtil.getDriver("chrome");
+        driver.manage().window().maximize();
 
+        driver.get("https://beta.docuport.app");
+
+        //forget password link
+        WebElement forgetPasswordLink = driver.findElement(By.cssSelector("a[href='/reset-password']"));
+        forgetPasswordLink.click();
+
+        WebElement forgetPasswordHeading = driver.findElement(By.cssSelector("div[class='login-layout_form']>h1"));
+        System.out.println("forgetPasswordHeading.getText() = "+ forgetPasswordHeading.getText() );
     }
 }
